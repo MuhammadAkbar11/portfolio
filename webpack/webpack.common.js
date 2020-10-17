e7241132247c57b55056e5c72f9ff7806fdedc0d
Unit-Git <theunit954@gmail.com>
2020-10-17 17:17:30 +0700

    change dev port to 9000

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions: ['*', '.js', '.jsx', '.json'],
   },
   devServer: {
-    port: 8080,
+    port: 9000,
     contentBase: path.resolve(__dirname, '../', 'dist'),
     hot: true,
     open: true,
