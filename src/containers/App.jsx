08120e3bb1d18c2fe12b4667673443060d61d966
Unit-Git <theunit954@gmail.com>
2020-10-18 08:26:58 +0700

    Adding react-router-dom and setup routes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+
 const App = () => {
   return (
-    <div className='px-4 h-screen w-full flex flex-col justify-center items-center bg-gray-300 '>
-      <h2 className='text-center text-2xl lg:text-4xl font-bold '>
-        React, Webpack and Babel Boilerplate{' '}
-      </h2>
-      <p className='lg:text-2xl mt-2 text-gray-600'>
-        And Support <span className='text-teal-600 fons'>TailwindCss</span>
-      </p>
-      <p className=' mt-2 text-gray-800 text-center '>
-        A Starter project using React, Webpack, babel and TailwindCss Setup
-      </p>
-    </div>
+    <Router>
+      <Route path='/' exact component={Home} />
+    </Router>
   );
 };
 
