cffa299630c44683bd3d2366bfd91b593e7ab686
Unit-Git <theunit954@gmail.com>
2020-10-18 13:28:20 +0700

    update output file name

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
+const path = require('path')
+
 const tailwindcss = require('tailwindcss');
 module.exports = {
   syntax: 'postcss-scss',
   plugins: [
-    require('postcss-import'),
+    require('postcss-import')({
+      path : path.resolve(__dirname, "src/index.scss"),
+    }),
     tailwindcss('./tailwind.config.js'),
     require('autoprefixer'),
   ],
