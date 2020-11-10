354f9177f3c09b12e476a85f100bd8ac0baafe57
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-11-10 14:29:12 +0700

    add title

--- src/app/containers/Home.jsx
+++ src/app/containers/Home.jsx
@@ -1,15 +1,21 @@
 import React, { useContext } from 'react';
 
 import { PublicContext } from '@app/context/context';
-import { Template, Hero } from '@components';
+import { Template, Hero, Socials } from '@components';
+import { useTitle } from '../hooks';
 
 const Home = () => {
   const context = useContext(PublicContext); // eslint-disable-line
+
+  // useEffect(() => {
+  useTitle('Home');
+  // }, []);
 
   return (
     <>
       <Template>
         <Hero />
+        <Socials />
       </Template>
     </>
   );
