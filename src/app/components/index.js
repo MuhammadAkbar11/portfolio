c7a1530a5465b820c82cbcafb766fc8a25d957d2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-11-11 17:03:35 +0700

    import page title

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -2,6 +2,9 @@
 export { default as Template } from './layouts/template';
 export { default as SideWrapper } from './layouts/sidewrap';
 export { default as TopBar } from './layouts/topbar';
+export { default as TitleOverlay } from './layouts/pageTitleOverlay';
+export { default as PageTitle } from './layouts/pageTitle';
+
 export { default as Hero } from './hero/hero';
 export { default as ButtonLink } from './buttonLink';
 export { default as Button } from './button';
