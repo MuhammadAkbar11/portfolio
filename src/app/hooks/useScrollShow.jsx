95e7bb85cb245fdb0c625d8d438a050e892e8966
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-11-16 20:23:45 +0700

    create useScroll Hook

---
+++ src/app/hooks/useScrollShow.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+const useScrollShow = () => {
+  const [isShow, setShow] = useState(false); // eslint-disable-line
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      setShow(true);
+    }
+  }, [inView, isShow]);
+
+  return [ref, isShow];
+};
+
+export default useScrollShow;
