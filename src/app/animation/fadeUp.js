4066b0b3cf7fec140b3edffef245fae4d11cc478
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-11-16 20:21:17 +0700

    add beforeChildren transition

--- src/app/animation/fadeUp.js
+++ src/app/animation/fadeUp.js
@@ -6,6 +6,7 @@ const fadeUpVariants = delay => {
       y: 0,
       transition: {
         y: { stiffness: 1000, velocity: -100 },
+        when: 'beforeChildren',
         delay: 0.1 + delay,
         duration: 0.1,
       },
