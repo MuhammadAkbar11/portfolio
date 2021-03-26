505963cd6b1a830bf871be7cadc48204a1edcd99
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-26 14:54:42 +0700

    delete useState

--- src/app/components/layouts/topbar.jsx
+++ src/app/components/layouts/topbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import Logo from 'assets/svg/AL.svg';
