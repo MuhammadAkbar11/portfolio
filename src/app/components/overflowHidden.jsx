f51f920dff190e1146269bbbde34d1c6b4ea8a8c
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-02-16 11:44:03 +0700

    passing props on overflowHidden component

--- src/app/components/overflowHidden.jsx
+++ src/app/components/overflowHidden.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const overflowHidden = ({ children }) => {
+const overflowHidden = ({ children, ...props }) => {
+  const { className } = props;
+  delete props.className;
   return (
-    <div className=' flex relative inset-0 p-0 m-0 overflow-hidden'>
+    <div
+      className={` flex relative inset-0 p-0 m-0 overflow-hidden ${className}`}
+      {...props}
+    >
       {children}
     </div>
   );
