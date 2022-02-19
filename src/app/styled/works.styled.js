8bb9ac239d4d8a851f62a327ec0991c0bee9b1f4
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-02-19 20:41:39 +0700

    small change for some components

--- src/app/styled/works.styled.js
+++ src/app/styled/works.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 export const styledSelectedProjectSection = motion.custom(styled.section`
-  ${tw`h-full  flex flex-col min-sm:mt-20`}
+  ${tw`h-full  flex flex-col `}
   margin-top: 3rem;
   @media (min-width: 576px) {
     margin-top: 8rem;
