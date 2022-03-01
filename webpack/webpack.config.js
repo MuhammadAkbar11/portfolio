54b651f8ff43b151a4878473d07b64d87804d761
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-03-01 23:43:21 +0700

    fix env variable undefined when production build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,10 @@
 /* eslint-disable global-require */
 
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
-module.exports = ({ env }) => {
+module.exports = configs => {
+  const env = configs?.env ? configs.env : process.env.NODE_ENV;
   const envConfig = require(`./webpack.${env}.js`);
 
   return merge(commonConfig, envConfig);
