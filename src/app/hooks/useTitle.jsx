0930cc2fcc5205a1e7b4fbee8e13743865c51cba
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-03 23:55:02 +0700

    refactor page title for main page

--- src/app/hooks/useTitle.jsx
+++ src/app/hooks/useTitle.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const useTitle = title => {
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
   const [titleText, setTitle] = useState('');
   useEffect(() => {
     setTitle(title);
-    document.title = `Muhammad Akbar  | ${titleText}`;
+
+    if (isHome) {
+      document.title = `Muhammad Akbar`;
+    } else {
+      document.title = `Muhammad Akbar | ${titleText}`;
+    }
+
     return () => {
       setTitle('Loading');
       document.title = `${titleText}... Muhammad Akbar  | ..`;
     };
-  }, [titleText]);
+  }, [titleText, isHome]);
 };
 
 export default useTitle;
